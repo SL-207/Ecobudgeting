763cfc4f7064f968a4dd036f18fe6ad4d83714e2
SL-207 <sl207020@gmail.com>
2022-11-19 17:47:54 +0000

    Update2
    - settings activity and layout
    - theme updates

--- app/src/main/java/com/example/durhack2022/MainActivity.java
+++ app/src/main/java/com/example/durhack2022/MainActivity.java
@@ -4,20 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button formBtn, settingsBtn;
-    public static int ovenConsumptionfan, ovenConsumptionfanless;
+    public static int ovenConsumptionfan = 10, ovenConsumptionfanless = 10, elecPrice = 10, gasPrice = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ovenConsumptionfan = 10;
-        ovenConsumptionfanless = 20;
         formBtn = findViewById(R.id.formBtn);
         settingsBtn = findViewById(R.id.settingsBtn);
+
         formBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/durhack2022/SettingsActivity.java
+++ app/src/main/java/com/example/durhack2022/SettingsActivity.java
@@ -14,21 +14,37 @@ import org.w3c.dom.Text;
 
 public class SettingsActivity extends AppCompatActivity {
     Button saveBtn; //save settings
-    TextView settingsFieldOne, settingsFieldTwo;
+    EditText sFieldOne, sFieldTwo, sFieldThree, sFieldFour, sFieldFive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         saveBtn = findViewById(R.id.saveBtn);
-        settingsFieldOne = (TextView) findViewById(R.id.fieldOne);      //fan
-        settingsFieldTwo = (TextView) findViewById(R.id.fieldTwo);      //fanless
+        sFieldOne = findViewById(R.id.fieldOne);      //fan
+        sFieldTwo = findViewById(R.id.fieldTwo);      //fanless
+        sFieldThree = findViewById(R.id.fieldThree);    //hob
+        sFieldFour = findViewById(R.id.fieldFour);      //gas price
+        sFieldFive = findViewById(R.id.fieldFive);      //electricity price
+
+        sFieldOne.setText("" + MainActivity.ovenConsumptionfan);
+        sFieldTwo.setText("" + MainActivity.ovenConsumptionfanless);
+        sFieldThree.setText("" + MainActivity.ovenConsumptionfan);
+        sFieldFour.setText("" + MainActivity.gasPrice);
+        sFieldFive.setText("" + MainActivity.elecPrice);
+        sFieldOne.setSelectAllOnFocus(true);
+        sFieldTwo.setSelectAllOnFocus(true);
+        sFieldThree.setSelectAllOnFocus(true);
+        sFieldFour.setSelectAllOnFocus(true);
+        sFieldFive.setSelectAllOnFocus(true);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                MainActivity.ovenConsumptionfan = Integer.parseInt(settingsFieldOne.getText().toString());
-                MainActivity.ovenConsumptionfanless = Integer.parseInt(settingsFieldTwo.getText().toString());
+                MainActivity.ovenConsumptionfan = Integer.parseInt(((TextView) sFieldOne).getText().toString());
+                MainActivity.ovenConsumptionfanless = Integer.parseInt(((TextView) sFieldTwo).getText().toString());
+                MainActivity.gasPrice = Integer.parseInt(((TextView) sFieldFour).getText().toString());
+                MainActivity.elecPrice = Integer.parseInt(((TextView) sFieldFive).getText().toString());
                 Toast.makeText(SettingsActivity.this, "Settings saved", Toast.LENGTH_SHORT).show();
                 SettingsActivity.this.startActivity(intent);
             }
